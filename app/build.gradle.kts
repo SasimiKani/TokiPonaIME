9b1f6967c9e4a08dc771d1677ddbe4945fc59cf3
SasimiKani <kyu9iro@gmail.com>
2025-02-04 07:11:19 +0900

    README更新、バージョンβ付け

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tokiponaime.tokiponaime"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.4β"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
